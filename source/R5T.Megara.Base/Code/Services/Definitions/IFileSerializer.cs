ddfbb9d78f65aae3953657cb5c46e0560d4e8725
David Coats <david@getsafetycone.com>
2020-08-21 16:41:13 -0700

    Improve documentation

--- source/R5T.Megara.Base/Code/Services/Definitions/IFileSerializer.cs
+++ source/R5T.Megara.Base/Code/Services/Definitions/IFileSerializer.cs
@@ -4,13 +4,12 @@
 namespace R5T.Megara
 {
     /// <summary>
-    /// De/serializes an object to a file.
+    /// De/serializes an object to/from a file.
     /// </summary>
     /// <remarks>
     /// The details of what file format (BinaryFormatter, custom binary format, XML, text, etc.) are left unspecified.
     ///
     /// Note to implementers:
-    /// * Upon reading, you can assume the file exists, and it doesn't throw an exception.
     /// * Upon writing, note that the optional overwrite argument default value is true.
     /// </remarks>
     public interface IFileSerializer<T>
